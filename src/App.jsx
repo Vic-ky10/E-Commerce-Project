35e17be864289a4a3c2c37dcf363701e69ddc543
vigneshwaran <vigneshvenketasan@gmail.com>
2026-02-21 10:17:54 +0530

    upadted the jsx files

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
+import { Routes, Route } from "react-router";
 import "./App.css";
 import HomePage from "./Pages/HomePage";
+import CheckoutPage from "./Pages/CheckoutPage";
+import OrderPage from "./Pages/OrderPage";
+import TrackingPage from "./Pages/TrackingPage";
 
 function App() {
   return (
     <>
-      <HomePage />
+      <Routes>
+        <Route index element={<HomePage />} />
+        <Route path="/checkout" element={<CheckoutPage />} />
+        <Route path="/orders" element={<OrderPage />} />
+        <Route path="/tracking" element ={<TrackingPage />} />
+      </Routes>
     </>
   );
 }
